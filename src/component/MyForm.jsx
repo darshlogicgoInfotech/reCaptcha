9f847a99ef5d44192533810de155927f5af80edd
darshLogicgo <darsh.logicgo@gmail.com>
2025-03-21 14:12:52 +0530

    first commit

--- src/component/MyForm.jsx
+++ src/component/MyForm.jsx
@@ -8,7 +8,7 @@ const MyForm = () => {
     <form>
       <HCaptcha   
         sitekey="feba7428-13c0-43ee-852a-f91e7dc86794"
-        language="hi" // Spanish language set kar raha hai
+        data-language="ET"
         onVerify={(token) => setToken(token)}
       />
       <button type="submit" disabled={!token}>Submit</button>
