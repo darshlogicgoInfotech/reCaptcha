f8da31bfb5faec9f2472127835e25b32f586aa50
darshLogicgo <darsh.logicgo@gmail.com>
2025-03-21 14:15:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173
+    port: process.env.PORT || 5173,
+    allowedHosts: [
+      'captcha-test.onrender.com',
+      '.onrender.com'
+    ]
   }
 })
